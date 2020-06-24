8a1a0f79eb07ce47985685cfeba9db95cfaf0764
hota1024 <hotalog@hota1024.com>
2020-06-24 16:02:42 +0900

    Add AuthController

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -6,5 +6,74 @@ use Illuminate\Http\Request;
 
 class AuthController extends Controller
 {
-    //
+    /**
+     * AuthController constructor.
+     */
+    public function __construct()
+    {
+        $this->middleware('auth:api', ['except' => ['login']]);
+    }
+
+    /**
+     * Login user.
+     *
+     * @return \Illuminate\Http\JsonResponse
+     */
+    public function login()
+    {
+        $credentials = request(['email', 'password']);
+        $token = auth()->attempt($credentials);
+
+        if (!$token)
+        {
+            return response()->json([
+                'error' => 'Unauthorized',
+            ], 401);
+        }
+
+        return $this->respondWithToken($token);
+    }
+
+    /**
+     * Returns logged in user.
+     *
+     * @return \Illuminate\Http\JsonResponse
+     */
+    public function me()
+    {
+        return response()->json(auth()->user());
+    }
+
+    /**
+     * Logout logged in user.
+     */
+    public function logout()
+    {
+        auth()->logout();
+    }
+
+    /**
+     * Refresh user token.
+     *
+     * @return \Illuminate\Http\JsonResponse
+     */
+    public function refresh()
+    {
+        return $this->respondWithToken(auth()->refresh());
+    }
+
+    /**
+     * Returns response object with token.
+     *
+     * @param $token
+     * @return \Illuminate\Http\JsonResponse
+     */
+    private function respondWithToken($token)
+    {
+        return response()->json([
+            'access_token' => $token,
+            'token_type' => 'bearer',
+            'expires_in' => auth()->factory()->getTTL() * 60,
+        ]);
+    }
 }
