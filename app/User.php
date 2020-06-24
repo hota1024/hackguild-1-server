5630cd846c08b296e0fd14228d75235799778d72
hota1024 <hotalog@hota1024.com>
2020-06-24 15:32:02 +0900

    Update User model

--- app/User.php
+++ app/User.php
@@ -2,14 +2,20 @@
 
 namespace App;
 
+use App\Models\UuidModel;
 use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
+use Ramsey\Uuid\Uuid;
 use Tymon\JWTAuth\Contracts\JWTSubject;
 
 class User extends Authenticatable implements JWTSubject
 {
     use Notifiable;
+
+    protected $keyType = 'string';
+
+    public $incrementing = false;
 
     /**
      * The attributes that are mass assignable.
@@ -37,6 +43,13 @@ class User extends Authenticatable implements JWTSubject
     protected $casts = [
         'email_verified_at' => 'datetime',
     ];
+
+    public function __construct(array $attributes = [])
+    {
+        parent::__construct($attributes);
+
+        $this->attributes['id'] = Uuid::uuid4()->toString();
+    }
 
     /**
      * Returns JWT identifier.
