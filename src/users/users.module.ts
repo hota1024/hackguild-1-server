5be4925071326289eff81e0818f8127ce3ebcea1
hota1024 <hotalog@hota1024.com>
2020-06-25 16:49:27 +0900

    🚨 Fix users module

--- src/users/users.module.ts
+++ src/users/users.module.ts
@@ -2,7 +2,7 @@ import { Module } from '@nestjs/common'
 import { UsersService } from './users.service'
 import { TypeOrmModule } from '@nestjs/typeorm'
 import { User } from './user.entity'
-import { UsersController } from './users.controller';
+import { UsersController } from './users.controller'
 
 @Module({
   imports: [TypeOrmModule.forFeature([User])],
