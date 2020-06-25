2a9eed83793f799cdec170373a44fc17bb1e1f6e
hota1024 <hotalog@hota1024.com>
2020-06-25 16:54:03 +0900

    🏷️ Add userRegister.dto.ts

---
+++ src/users/dto/userRegister.dto.ts
@@ -0,0 +1,38 @@
+import { IsNotEmpty, IsString, IsEmail, IsOptional } from 'class-validator'
+import { IsUnique } from 'src/validators'
+import { User } from '../user.entity'
+
+/*
+ * User register DTO.
+ */
+export class UserRegisterDto {
+  /**
+   * Name.
+   */
+  @IsNotEmpty()
+  @IsString()
+  @IsUnique(User)
+  name: string
+
+  /**
+   * Display name(default is name value.).
+   */
+  @IsString()
+  @IsOptional()
+  displayName?: string
+
+  /**
+   * Email.
+   */
+  @IsNotEmpty()
+  @IsEmail()
+  @IsUnique(User)
+  email: string
+
+  /**
+   * Password.
+   */
+  @IsNotEmpty()
+  @IsString()
+  password: string
+}
