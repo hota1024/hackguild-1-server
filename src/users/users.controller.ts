f5e671af2c9b3586caf77b55edb745479549a7d2
hota1024 <hotalog@hota1024.com>
2020-06-25 16:48:41 +0900

    🏷️ Add users controller

---
+++ src/users/users.controller.ts
@@ -0,0 +1,31 @@
+import { Controller, Post, Body, Inject } from '@nestjs/common'
+import { UserRegisterDto } from './users.dto'
+import { UsersService } from './users.service'
+import { User } from './user.entity'
+
+@Controller('users')
+export class UsersController {
+  /**
+   * Users service.
+   */
+  private readonly usersService: UsersService
+
+  /**
+   * UsersController constructor.
+   *
+   * @param usersService Users service(DI).
+   */
+  constructor(usersService: UsersService) {
+    this.usersService = usersService
+  }
+
+  /**
+   * Register a user.
+   *
+   * @param data Data.
+   */
+  @Post()
+  async register(@Body() data: UserRegisterDto): Promise<User> {
+    return await this.usersService.register(data)
+  }
+}
