273eed7de4b974fbb1384af2865171b3427a36c6
hota1024 <hotalog@hota1024.com>
2020-06-26 10:24:09 +0900

    🏷️ Update auth controll.er

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,6 +1,7 @@
 import { Controller, UseGuards, Post, Request } from '@nestjs/common'
 import { AuthGuard } from '@nestjs/passport'
 import { AuthService } from './auth.service'
+import { RequestWithUser } from 'src/types'
 
 @Controller('auth')
 export class AuthController {
@@ -23,7 +24,15 @@ export class AuthController {
    */
   @UseGuards(AuthGuard('local'))
   @Post('/login')
-  login(@Request() request) {
-    return this.authService.login(request.user)
+  async login(
+    @Request() request: RequestWithUser
+  ): Promise<{
+    accessToken: string
+  }> {
+    const accessToken = await this.authService.login(request.user)
+
+    return {
+      accessToken,
+    }
   }
 }
