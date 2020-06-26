4d254494bd0a48cfdd1bcbba0f358ad1abdcbe7a
hota1024 <hotalog@hota1024.com>
2020-06-26 10:03:34 +0900

    🏷️ Update auth module

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,13 +1,25 @@
 import { Module } from '@nestjs/common'
+import { PassportModule } from '@nestjs/passport'
+import { JwtModule } from '@nestjs/jwt'
 import { AuthService } from './auth.service'
 import { AuthController } from './auth.controller'
-import { PassportModule } from '@nestjs/passport'
 import { LocalStrategy } from './local.strategy'
 import { UsersModule } from 'src/users/users.module'
+import { JwtStrategy } from './jwt.strategy'
+import { getConfig } from 'src/config'
 
 @Module({
-  imports: [PassportModule, UsersModule],
-  providers: [AuthService, LocalStrategy],
+  imports: [
+    PassportModule,
+    UsersModule,
+    JwtModule.register({
+      secret: getConfig('JWT_SECRET'),
+      signOptions: {
+        expiresIn: '1w',
+      },
+    }),
+  ],
+  providers: [AuthService, LocalStrategy, JwtStrategy],
   controllers: [AuthController],
 })
 export class AuthModule {}
