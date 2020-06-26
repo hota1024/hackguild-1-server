df3f946048d9c676f7263ccd9dadf4c5976d584c
hota1024 <hotalog@hota1024.com>
2020-06-26 09:58:30 +0900

    🏷️ Update app module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,22 +1,21 @@
 import * as path from 'path'
 import { Module } from '@nestjs/common'
-import { ConfigModule } from '@nestjs/config'
 import { TypeOrmModule } from '@nestjs/typeorm'
 import { AppController } from './app.controller'
 import { AppService } from './app.service'
 import { UsersModule } from './users/users.module'
 import { AuthModule } from './auth/auth.module'
+import { getConfig } from './config'
 
 @Module({
   imports: [
-    ConfigModule.forRoot(),
     TypeOrmModule.forRoot({
       type: 'mysql',
-      host: process.env.DB_HOST,
-      port: parseInt(process.env.DB_PORT),
-      username: process.env.DB_USER,
-      password: process.env.DB_PASSWORD,
-      database: process.env.DB_NAME,
+      host: getConfig('DB_HOST'),
+      port: getConfig('DB_PORT'),
+      username: getConfig('DB_USER'),
+      password: getConfig('DB_PASSWORD'),
+      database: getConfig('DB_NAME'),
       entities: [path.join(__dirname, '**/**.entity{.ts,.js}')],
       synchronize: true,
     }),
